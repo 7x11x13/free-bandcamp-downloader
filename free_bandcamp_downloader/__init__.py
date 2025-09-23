46f6a7bff7571dcd4c2d4198542cdaeeebbc25fd
absolutesolver336699xx-sys <absolutesolver336699xx@gmail.com>
2025-09-23 16:30:46 -0300

    Update __init__.py

--- free_bandcamp_downloader/__init__.py
+++ free_bandcamp_downloader/__init__.py
@@ -1,8 +1,2 @@
 import importlib.metadata
-import logging
-import os
-
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-logger = logging.getLogger(__name__)
-
 __version__ = importlib.metadata.version("free-bandcamp-downloader")
